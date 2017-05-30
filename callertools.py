629671a1da8b1bd1638a79c713d3065524de41dc
Kokitis <sypilus@gmail.com>
2017-05-30 14:44:09 -0400

    Major Updates

--- callertools.py
+++ callertools.py
@@ -2,8 +2,11 @@
 import os
 import re
 from pprint import pprint
-GITHUB_FOLDER = os.path.join(os.getenv('USERPROFILE'), 'Documents', 'Github')
-print(GITHUB_FOLDER)
+if os.name == 'nt':
+	GITHUB_FOLDER = os.path.join(os.getenv('USERPROFILE'), 'Documents', 'Github')
+else:
+	GITHUB_FOLDER = os.path.join(os.getenv('HOME'), 'Documents', 'Github')
+
 import sys
 sys.path.append(GITHUB_FOLDER)
 import pytools.filetools as filetools
@@ -21,6 +24,11 @@ def getOutputCaller(filename):
 class CallerOutputClassifier:
 	"""
 		Searches a patient's folder for the relevant caller outputs.
+		Keyword Arguments
+		-----------------
+			'reduce': bool; default False
+				if 'True', will only return a single result.
+			'verbose': bool; default False
 		Returns
 		-------
 			outputs: dict<>
@@ -36,18 +44,26 @@ class CallerOutputClassifier:
 			classifier = CallerOutputClassifier()
 			result = classifier(patient_folder)
 	"""
-	def __init__(self):
+	def __init__(self, **kwargs):
+		self.return_single_result = kwargs.get('reduce', False)
+		self.verbose = kwargs.get('verbose', False)
 		self.suffixes = self._defineCallerIdentifiers()
-	def __call__(self, folder):
+	def __call__(self, folder, **kwargs):
 		""" Parameters
 			----------
 				folder: the patient's folder
 		"""
-		file_list = filetools.listAllFiles(folder)
+		file_list = filetools.listAllFiles(folder, **kwargs)
 
 		caller_outputs = self._searchFolder(file_list)
+		if self.return_single_result:
+			caller_outputs = self._reduce(caller_outputs)
+		if self.verbose:
+			pprint(caller_outputs)
 		return caller_outputs
-		return self._searchFolder(folder)
+	def _reduce(self, outputs):
+		outputs = {k:(v if isinstance(v, str) else v[0]) for k, v in outputs.items()}
+		return outputs
 	def _checkSuffixes(self, filename, suffixes):
 		""" Checks the suffixes to see if they're a match """
 		is_caller = any(filename.endswith(s) for s in suffixes)
@@ -60,8 +76,8 @@ class CallerOutputClassifier:
 			'mutect2': 			['.mutect2.vcf'],
 			'somaticsniper': 	['.somaticsniper.vcf'],
 			'strelka-indel': 	['.passed.somatic.indels.vcf.strelka.vcf'],
-			'strelka-snv': 		['.passed.somatic.snvs.vcf.strelka.vcf'],
-			'varscan-snv': 		['.raw.indel.vcf'],
+			'strelka-snp': 		['.passed.somatic.snvs.vcf.strelka.vcf'],
+			'varscan-snp': 		['.raw.indel.vcf'],
 			'varscan-indel': 	['.raw.snp.vcf']
 		}
 		return suffixes
@@ -80,16 +96,19 @@ class CallerOutputClassifier:
 		result = dict()
 		for filename in file_list:
 			caller = self._getOutputCallers(filename)
+			#print(caller, '\t', filename)
 			if caller is None: continue
 			elif caller in result:
 				result[caller].append(filename)
 			else:
 				result[caller] = [filename]
+
+
 		return result
 
 if __name__ == "__main__":
-	folder = "G:\\Data\\TCGA-ESCA\\raw_snp_output\\TCGA-2H-A9GF\\"
-	_test = CallerOutput()
-	result = _test(folder)
+	folder = "/home/upmc/Documents/Genomic_Analysis/1_input_vcfs/original_callsets/TCGA-2H-A9GF"
+	_test = CallerOutputClassifier()
+	result = _test(folder, exclude = ['strelka', 'chromosome'], logic = 'and')
 	pprint(result)
-print("Finished!")
+	print("Finished!")
